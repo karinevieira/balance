151378eef0491baadf0cee9bf6fded97195428e7
Karine <62672837+karinevieira@users.noreply.github.com>
2024-01-03 15:42:16 -0300

    feat: lists profiles

--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resource :profile, only: %i[show update]
+      resources :profiles, only: %i[index show update]
     end
   end
 end
--- spec/routing/api/v1/profiles_routing_spec.rb
+++ spec/routing/api/v1/profiles_routing_spec.rb
@@ -3,8 +3,9 @@
 require "rails_helper"
 
 RSpec.describe Api::V1::ProfilesController do
-  it { is_expected.to route(:get, "/api/v1/profile").to(controller: "api/v1/profiles", action: :show) }
+  it { is_expected.to route(:get, "/api/v1/profiles").to(controller: "api/v1/profiles", action: :index) }
+  it { is_expected.to route(:get, "/api/v1/profiles/1").to(id: "1", controller: "api/v1/profiles", action: :show) }
 
-  it { is_expected.to route(:put, "/api/v1/profile").to(controller: "api/v1/profiles", action: :update) }
-  it { is_expected.to route(:patch, "/api/v1/profile").to(controller: "api/v1/profiles", action: :update) }
+  it { is_expected.to route(:put, "/api/v1/profiles/1").to(id: "1", controller: "api/v1/profiles", action: :update) }
+  it { is_expected.to route(:patch, "/api/v1/profiles/1").to(id: "1", controller: "api/v1/profiles", action: :update) }
 end
