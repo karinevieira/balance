054e900448a73da179b950ae437d7fced46cc6e7
Karine Vieira <kv.vieira1@gmail.com>
2024-02-10 14:40:46 -0300

    refactor: remove authentication for exercise metrics endpoints

--- spec/requests/api/v1/exercise_metrics_controller_spec.rb
+++ spec/requests/api/v1/exercise_metrics_controller_spec.rb
@@ -25,162 +25,116 @@ RSpec.describe Api::V1::ExerciseMetricsController do
   end
 
   describe "POST #create" do
-    # rubocop:disable RSpec/NestedGroups
-    context "when the user is authenticated" do
-      context "when attributes are valid" do
-        it "renders a created response" do
-          jwt_token = sign_in
+    context "when attributes are valid" do
+      it "renders a created response" do
+        user = create(:user)
 
-          attributes = {
-            name: "test walk", steps: 1000, distance_in_m: 1000, intensity: "high"
-          }
-
-          post api_v1_exercise_metrics_path, headers: { authorization: jwt_token },
-            params: { exercise_metric: attributes }, as: :json
-
-          expect(response).to have_http_status(:created)
-        end
-
-        it "creates a new exercise metric for user" do
-          user = create(:user)
-          jwt_token = sign_in(user)
-
-          attributes = {
-            name: "test walk", steps: 1000, distance_in_m: 1000, intensity: "high"
-          }
-
-          post api_v1_exercise_metrics_path, headers: { authorization: jwt_token },
-            params: { exercise_metric: attributes }, as: :json
-
-          expect(user.reload.exercise_metrics.last.attributes).to include(
-            "name" => "test walk",
-            "steps" => 1000,
-            "distance_in_m" => 1000,
-            "intensity" => "high"
-          )
-        end
-      end
-
-      context "when attributes are invalid" do
-        it "renders an unprocessable_entity response" do
-          jwt_token = sign_in
-
-          attributes = {
-            name: nil, steps: 1000, distance_in_m: 1000, intensity: nil
-          }
-
-          post api_v1_exercise_metrics_path, headers: { authorization: jwt_token },
-            params: { exercise_metric: attributes }, as: :json
-
-          expect(response).to have_http_status(:unprocessable_entity)
-        end
-
-        it "doesn't create a new exercise metric for user" do
-          user = create(:user)
-          jwt_token = sign_in(user)
-
-          attributes = {
-            name: nil, steps: 1000, distance_in_m: 1000, intensity: nil
-          }
-
-          post api_v1_exercise_metrics_path, headers: { authorization: jwt_token },
-            params: { exercise_metric: attributes }, as: :json
-
-          expect(user.reload.exercise_metrics).to be_empty
-        end
-      end
-    end
-    # rubocop:enable RSpec/NestedGroups
-
-    context "when the user is not authenticated" do
-      it "renders an unauthorized response" do
         attributes = {
           name: "test walk", steps: 1000, distance_in_m: 1000, intensity: "high"
         }
 
-        post api_v1_exercise_metrics_path, headers: { authorization: nil },
-          params: { exercise_metric: attributes }, as: :json
+        post api_v1_exercise_metrics_path, params: { exercise_metric: attributes, user_id: user.id }, as: :json
 
-        expect(response).to have_http_status(:unauthorized)
+        expect(response).to have_http_status(:created)
+      end
+
+      it "creates a new exercise metric for user" do
+        user = create(:user)
+
+        attributes = {
+          name: "test walk", steps: 1000, distance_in_m: 1000, intensity: "high"
+        }
+
+        post api_v1_exercise_metrics_path, params: { exercise_metric: attributes, user_id: user.id }, as: :json
+
+        expect(user.reload.exercise_metrics.last.attributes).to include(
+          "name" => "test walk",
+          "steps" => 1000,
+          "distance_in_m" => 1000,
+          "intensity" => "high"
+        )
+      end
+    end
+
+    context "when attributes are invalid" do
+      it "renders an unprocessable_entity response" do
+        user = create(:user)
+
+        attributes = {
+          name: nil, steps: 1000, distance_in_m: 1000, intensity: nil
+        }
+
+        post api_v1_exercise_metrics_path, params: { exercise_metric: attributes, user_id: user.id }, as: :json
+
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
+
+      it "doesn't create a new exercise metric for user" do
+        user = create(:user)
+
+        attributes = {
+          name: nil, steps: 1000, distance_in_m: 1000, intensity: nil
+        }
+
+        post api_v1_exercise_metrics_path, params: { exercise_metric: attributes, user_id: user.id }, as: :json
+
+        expect(user.reload.exercise_metrics).to be_empty
       end
     end
   end
 
   describe "PUT #update" do
-    # rubocop:disable RSpec/NestedGroups
-    context "when the user is authenticated" do
-      context "when attributes are valid" do
-        it "renders a successful response" do
-          user = create(:user)
-          jwt_token = sign_in(user)
-          exercise_metric = create(:exercise_metric, user: user)
+    context "when attributes are valid" do
+      it "renders a successful response" do
+        user = create(:user)
+        exercise_metric = create(:exercise_metric, user: user)
 
-          attributes = { steps: 2000, distance_in_m: 2000 }
+        attributes = { steps: 2000, distance_in_m: 2000 }
 
-          put api_v1_exercise_metric_path(exercise_metric), headers: { authorization: jwt_token },
-            params: { exercise_metric: attributes }, as: :json
+        put api_v1_exercise_metric_path(exercise_metric),
+          params: { exercise_metric: attributes, user_id: user.id }, as: :json
 
-          expect(response).to be_successful
-        end
-
-        it "updates user's exercise metric" do
-          user = create(:user)
-          jwt_token = sign_in(user)
-          exercise_metric = create(:exercise_metric, name: "old test walk", user: user)
-
-          attributes = { name: "new test walk" }
-
-          put api_v1_exercise_metric_path(exercise_metric), headers: { authorization: jwt_token },
-            params: { exercise_metric: attributes }, as: :json
-
-          expect(user.reload.exercise_metrics.last.name).to eq("new test walk")
-        end
+        expect(response).to be_successful
       end
 
-      context "when attributes are invalid" do
-        it "renders an unprocessable_entity response" do
-          user = create(:user)
-          jwt_token = sign_in(user)
-          exercise_metric = create(:exercise_metric, user: user)
+      it "updates user's exercise metric" do
+        user = create(:user)
+        exercise_metric = create(:exercise_metric, name: "old test walk", user: user)
 
-          attributes = {
-            name: nil, steps: 1000, distance_in_m: 1000, intensity: nil
-          }
+        attributes = { name: "new test walk" }
 
-          put api_v1_exercise_metric_path(exercise_metric), headers: { authorization: jwt_token },
-            params: { exercise_metric: attributes }, as: :json
+        put api_v1_exercise_metric_path(exercise_metric),
+          params: { exercise_metric: attributes, user_id: user.id }, as: :json
 
-          expect(response).to have_http_status(:unprocessable_entity)
-        end
-
-        it "doesn't update user's exercise metric" do
-          user = create(:user)
-          jwt_token = sign_in(user)
-          exercise_metric = create(:exercise_metric, name: "old test walk", user: user)
-
-          attributes = { name: "new test walk", intensity: nil }
-
-          put api_v1_exercise_metric_path(exercise_metric), headers: { authorization: jwt_token },
-            params: { exercise_metric: attributes }, as: :json
-
-          expect(user.reload.exercise_metrics.last.name).to eq("old test walk")
-        end
+        expect(user.reload.exercise_metrics.last.name).to eq("new test walk")
       end
     end
-    # rubocop:enable RSpec/NestedGroups
 
-    context "when the user is not authenticated" do
-      it "renders an unauthorized response" do
-        exercise_metric = create(:exercise_metric)
+    context "when attributes are invalid" do
+      it "renders an unprocessable_entity response" do
+        user = create(:user)
+        exercise_metric = create(:exercise_metric, user: user)
 
         attributes = {
-          name: "test walk", steps: 1000, distance_in_m: 1000, intensity: "high"
+          name: nil, steps: 1000, distance_in_m: 1000, intensity: nil
         }
 
-        put api_v1_exercise_metric_path(exercise_metric), headers: { authorization: nil },
-          params: { exercise_metric: attributes }, as: :json
+        put api_v1_exercise_metric_path(exercise_metric),
+          params: { exercise_metric: attributes, user_id: user.id }, as: :json
 
-        expect(response).to have_http_status(:unauthorized)
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
+
+      it "doesn't update user's exercise metric" do
+        user = create(:user)
+        exercise_metric = create(:exercise_metric, name: "old test walk", user: user)
+
+        attributes = { name: "new test walk", intensity: nil }
+
+        put api_v1_exercise_metric_path(exercise_metric),
+          params: { exercise_metric: attributes, user_id: user.id }, as: :json
+
+        expect(user.reload.exercise_metrics.last.name).to eq("old test walk")
       end
     end
   end
