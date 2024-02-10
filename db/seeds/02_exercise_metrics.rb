b80eac6755ab4c938262ac6df253d3f4099e0053
Karine Vieira <kv.vieira1@gmail.com>
2024-02-10 15:17:05 -0300

    feat: add duration_in_min field to exercise_metrics table

--- db/seeds/02_exercise_metrics.rb
+++ db/seeds/02_exercise_metrics.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-Rails.logger.debug "\t Seeding crowdfunding projects data..."
+Rails.logger.debug "\t Seeding exercise metrics data..."
 
 user = User.find_by!(email: "isabella_galvao@limao.com.br")
 
@@ -10,49 +10,56 @@ user = User.find_by!(email: "isabella_galvao@limao.com.br")
     name: "Caminhada",
     steps: 2000,
     distance_in_m: 1520,
-    intensity: "normal"
+    intensity: "normal",
+    duration_in_min: 60
   },
   {
     user: user,
     name: "Caminhada",
     steps: 5000,
     distance_in_m: 3810,
-    intensity: "high"
+    intensity: "high",
+    duration_in_min: 60
   },
   {
     user: user,
     name: "Corrida",
     steps: 10_000,
     distance_in_m: 7620,
-    intensity: "high"
+    intensity: "high",
+    duration_in_min: 60
   },
   {
     user: user,
     name: "Caminhada",
     steps: 700,
     distance_in_m: 530,
-    intensity: "low"
+    intensity: "low",
+    duration_in_min: 15
   },
   {
     user: user,
     name: "Caminhada",
     steps: 6000,
     distance_in_m: 4570,
-    intensity: "high"
+    intensity: "high",
+    duration_in_min: 60
   },
   {
     user: user,
     name: "Corrida",
     steps: 12_000,
     distance_in_m: 9140,
-    intensity: "high"
+    intensity: "high",
+    duration_in_min: 60
   },
   {
     user: user,
     name: "Caminhada",
     steps: 500,
     distance_in_m: 380,
-    intensity: "low"
+    intensity: "low",
+    duration_in_min: 15
   }
 ].each do |data|
   ExerciseMetric.create!(data)
--- spec/models/exercise_metric_spec.rb
+++ spec/models/exercise_metric_spec.rb
@@ -13,5 +13,6 @@ RSpec.describe ExerciseMetric do
 
     it { is_expected.to validate_numericality_of(:steps).is_greater_than_or_equal_to(0) }
     it { is_expected.to validate_numericality_of(:distance_in_m).is_greater_than_or_equal_to(0) }
+    it { is_expected.to validate_numericality_of(:duration_in_min).is_greater_than_or_equal_to(0) }
   end
 end
