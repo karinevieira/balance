3c8698592c7f2e57a4288a36e76a564661116c87
Karine Vieira <62672837+karinevieira@users.noreply.github.com>
2024-03-09 11:01:14 -0300

    fix: add model validations to AccelerometerMetric (#37)

--- app/models/accelerometer_metric.rb
+++ app/models/accelerometer_metric.rb
@@ -2,4 +2,10 @@
 
 class AccelerometerMetric < ApplicationRecord
   belongs_to :user
+
+  validates :exercise, presence: true
+  validates :time_in_min, presence: true
+  validates :level, presence: true
+
+  validates :time_in_min, numericality: { greater_than_or_equal_to: 0 }
 end
--- spec/models/accelerometer_metric_spec.rb
+++ spec/models/accelerometer_metric_spec.rb
@@ -6,4 +6,12 @@ RSpec.describe AccelerometerMetric do
   describe "associations" do
     it { is_expected.to belong_to(:user) }
   end
+
+  describe "validations" do
+    it { is_expected.to validate_presence_of(:exercise) }
+    it { is_expected.to validate_presence_of(:time_in_min) }
+    it { is_expected.to validate_presence_of(:level) }
+
+    it { is_expected.to validate_numericality_of(:time_in_min).is_greater_than_or_equal_to(0) }
+  end
 end
