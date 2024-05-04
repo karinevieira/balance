99cd25c048d1e36f162edd83c97a20335bdfaacd
Karine Vieira <62672837+karinevieira@users.noreply.github.com>
2024-05-04 10:57:43 -0300

    feat: Make available accelerometer created_at date (#39)

--- app/serializers/accelerometer_metric_serializer.rb
+++ app/serializers/accelerometer_metric_serializer.rb
@@ -2,5 +2,5 @@
 
 class AccelerometerMetricSerializer
   include JSONAPI::Serializer
-  attributes :user_id, :exercise, :time_in_min, :level
+  attributes :user_id, :exercise, :time_in_min, :level, :created_at
 end
