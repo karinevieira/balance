29a3d1b853861c40b1ab602e182718780eb512b6
Karine Vieira <kv.vieira1@gmail.com>
2024-08-19 18:21:33 -0300

    config: Remove seeds data

--- db/seeds/01_users.rb
+++ db/seeds/01_users.rb
@@ -1,72 +1,72 @@
 # frozen_string_literal: true
 
-Rails.logger.debug "\t Seeding users data..."
+# Rails.logger.debug "\t Seeding users data..."
 
-[
-  {
-    name: "Isabella Galvão",
-    email: "isabella_galvao@limao.com.br",
-    password: "password",
-    profile_attributes: {
-      weight: 60,
-      height_in_cm: 160,
-      workout_in_min: 60,
-      workout_days_frequency: 3,
-      active_lifestyle: true,
-      gender: "female"
-    }
-  },
-  {
-    name: "César Ribeiro",
-    email: "cesar_ribeiro@msds.com.br",
-    password: "password",
-    profile_attributes: {
-      weight: 90,
-      height_in_cm: 190,
-      workout_in_min: 100,
-      workout_days_frequency: 5,
-      active_lifestyle: true,
-      gender: "male"
-    }
-  },
-  {
-    name: "Yuri Fogaça",
-    email: "yuri-fogaca95@grupoarteoficio.com.br",
-    password: "password",
-    profile_attributes: {
-      weight: 85,
-      height_in_cm: 170,
-      workout_in_min: 70,
-      workout_days_frequency: 4,
-      gender: "male"
-    }
-  },
-  {
-    name: "Rafaela Melo",
-    email: "rafaelacarolinamelo@cielo.com.br",
-    password: "password",
-    profile_attributes: {
-      weight: 45,
-      height_in_cm: 153,
-      workout_in_min: 60,
-      workout_days_frequency: 3,
-      gender: "female"
-    }
-  },
-  {
-    name: "Andreia Duarte",
-    email: "andreia.daiane.duarte@oliveiracontabil.com.br",
-    password: "password",
-    profile_attributes: {
-      weight: 55,
-      height_in_cm: 160,
-      workout_in_min: 60,
-      workout_days_frequency: 3,
-      gender: "female"
-    }
-  }
-].each do |data|
-  user = User.find_or_initialize_by(email: data[:email])
-  user.assign_attributes(data.slice(:name, :password, :profile_attributes))
-  user.save!
-end
+# [
+#   {
+#     name: "Isabella Galvão",
+#     email: "isabella_galvao@limao.com.br",
+#     password: "password",
+#     profile_attributes: {
+#       weight: 60,
+#       height_in_cm: 160,
+#       workout_in_min: 60,
+#       workout_days_frequency: 3,
+#       active_lifestyle: true,
+#       gender: "female"
+#     }
+#   },
+#   {
+#     name: "César Ribeiro",
+#     email: "cesar_ribeiro@msds.com.br",
+#     password: "password",
+#     profile_attributes: {
+#       weight: 90,
+#       height_in_cm: 190,
+#       workout_in_min: 100,
+#       workout_days_frequency: 5,
+#       active_lifestyle: true,
+#       gender: "male"
+#     }
+#   },
+#   {
+#     name: "Yuri Fogaça",
+#     email: "yuri-fogaca95@grupoarteoficio.com.br",
+#     password: "password",
+#     profile_attributes: {
+#       weight: 85,
+#       height_in_cm: 170,
+#       workout_in_min: 70,
+#       workout_days_frequency: 4,
+#       gender: "male"
+#     }
+#   },
+#   {
+#     name: "Rafaela Melo",
+#     email: "rafaelacarolinamelo@cielo.com.br",
+#     password: "password",
+#     profile_attributes: {
+#       weight: 45,
+#       height_in_cm: 153,
+#       workout_in_min: 60,
+#       workout_days_frequency: 3,
+#       gender: "female"
+#     }
+#   },
+#   {
+#     name: "Andreia Duarte",
+#     email: "andreia.daiane.duarte@oliveiracontabil.com.br",
+#     password: "password",
+#     profile_attributes: {
+#       weight: 55,
+#       height_in_cm: 160,
+#       workout_in_min: 60,
+#       workout_days_frequency: 3,
+#       gender: "female"
+#     }
+#   }
+# ].each do |data|
+#   user = User.find_or_initialize_by(email: data[:email])
+#   user.assign_attributes(data.slice(:name, :password, :profile_attributes))
+#   user.save!
+# end
--- db/seeds/02_exercise_metrics.rb
+++ db/seeds/02_exercise_metrics.rb
@@ -1,67 +1,67 @@
 # frozen_string_literal: true
 
-Rails.logger.debug "\t Seeding exercise metrics data..."
+# Rails.logger.debug "\t Seeding exercise metrics data..."
 
-user = User.find_by!(email: "isabella_galvao@limao.com.br")
-user.exercise_metrics.destroy_all
+# user = User.find_by!(email: "isabella_galvao@limao.com.br")
+# user.exercise_metrics.destroy_all
 
-[
-  {
-    user: user,
-    name: "Caminhada",
-    steps: 2000,
-    distance_in_m: 1520,
-    intensity: "normal",
-    duration_in_min: 60
-  },
-  {
-    user: user,
-    name: "Caminhada",
-    steps: 5000,
-    distance_in_m: 3810,
-    intensity: "high",
-    duration_in_min: 60
-  },
-  {
-    user: user,
-    name: "Corrida",
-    steps: 10_000,
-    distance_in_m: 7620,
-    intensity: "high",
-    duration_in_min: 60
-  },
-  {
-    user: user,
-    name: "Caminhada",
-    steps: 700,
-    distance_in_m: 530,
-    intensity: "low",
-    duration_in_min: 15
-  },
-  {
-    user: user,
-    name: "Caminhada",
-    steps: 6000,
-    distance_in_m: 4570,
-    intensity: "high",
-    duration_in_min: 60
-  },
-  {
-    user: user,
-    name: "Corrida",
-    steps: 12_000,
-    distance_in_m: 9140,
-    intensity: "high",
-    duration_in_min: 60
-  },
-  {
-    user: user,
-    name: "Caminhada",
-    steps: 500,
-    distance_in_m: 380,
-    intensity: "low",
-    duration_in_min: 15
-  }
-].each do |data|
-  ExerciseMetric.create!(data)
-end
+# [
+#   {
+#     user: user,
+#     name: "Caminhada",
+#     steps: 2000,
+#     distance_in_m: 1520,
+#     intensity: "normal",
+#     duration_in_min: 60
+#   },
+#   {
+#     user: user,
+#     name: "Caminhada",
+#     steps: 5000,
+#     distance_in_m: 3810,
+#     intensity: "high",
+#     duration_in_min: 60
+#   },
+#   {
+#     user: user,
+#     name: "Corrida",
+#     steps: 10_000,
+#     distance_in_m: 7620,
+#     intensity: "high",
+#     duration_in_min: 60
+#   },
+#   {
+#     user: user,
+#     name: "Caminhada",
+#     steps: 700,
+#     distance_in_m: 530,
+#     intensity: "low",
+#     duration_in_min: 15
+#   },
+#   {
+#     user: user,
+#     name: "Caminhada",
+#     steps: 6000,
+#     distance_in_m: 4570,
+#     intensity: "high",
+#     duration_in_min: 60
+#   },
+#   {
+#     user: user,
+#     name: "Corrida",
+#     steps: 12_000,
+#     distance_in_m: 9140,
+#     intensity: "high",
+#     duration_in_min: 60
+#   },
+#   {
+#     user: user,
+#     name: "Caminhada",
+#     steps: 500,
+#     distance_in_m: 380,
+#     intensity: "low",
+#     duration_in_min: 15
+#   }
+# ].each do |data|
+#   ExerciseMetric.create!(data)
+# end
