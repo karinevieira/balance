beab449eb45e7c0371e2f45c9792d40ae2d2c103
Karine Vieira <62672837+karinevieira@users.noreply.github.com>
2024-08-19 18:23:26 -0300

    config: remove seeds data (#41)

--- db/seeds/03_accelerometer_metrics.rb
+++ db/seeds/03_accelerometer_metrics.rb
@@ -1,53 +1,53 @@
 # frozen_string_literal: true
 
-Rails.logger.debug "\t Seeding accelerometer metrics data..."
+# Rails.logger.debug "\t Seeding accelerometer metrics data..."
 
-user = User.find_by!(email: "isabella_galvao@limao.com.br")
-user.accelerometer_metrics.destroy_all
+# user = User.find_by!(email: "isabella_galvao@limao.com.br")
+# user.accelerometer_metrics.destroy_all
 
-[
-  {
-    user: user,
-    exercise: "Caminhada",
-    level: "Sedentário",
-    time_in_min: 0
-  },
-  {
-    user: user,
-    exercise: "Caminhada",
-    level: "Sedentário",
-    time_in_min: 3
-  },
-  {
-    user: user,
-    exercise: "Corrida",
-    level: "Sedentário",
-    time_in_min: 0
-  },
-  {
-    user: user,
-    exercise: "Caminhada",
-    level: "Sedentário",
-    time_in_min: 3
-  },
-  {
-    user: user,
-    exercise: "Caminhada",
-    level: "Sedentário",
-    time_in_min: 0
-  },
-  {
-    user: user,
-    exercise: "Corrida",
-    level: "Sedentário",
-    time_in_min: 3
-  },
-  {
-    user: user,
-    exercise: "Caminhada",
-    level: "Sedentário",
-    time_in_min: 0
-  }
-].each do |data|
-  AccelerometerMetric.create!(data)
-end
+# [
+#   {
+#     user: user,
+#     exercise: "Caminhada",
+#     level: "Sedentário",
+#     time_in_min: 0
+#   },
+#   {
+#     user: user,
+#     exercise: "Caminhada",
+#     level: "Sedentário",
+#     time_in_min: 3
+#   },
+#   {
+#     user: user,
+#     exercise: "Corrida",
+#     level: "Sedentário",
+#     time_in_min: 0
+#   },
+#   {
+#     user: user,
+#     exercise: "Caminhada",
+#     level: "Sedentário",
+#     time_in_min: 3
+#   },
+#   {
+#     user: user,
+#     exercise: "Caminhada",
+#     level: "Sedentário",
+#     time_in_min: 0
+#   },
+#   {
+#     user: user,
+#     exercise: "Corrida",
+#     level: "Sedentário",
+#     time_in_min: 3
+#   },
+#   {
+#     user: user,
+#     exercise: "Caminhada",
+#     level: "Sedentário",
+#     time_in_min: 0
+#   }
+# ].each do |data|
+#   AccelerometerMetric.create!(data)
+# end
